165a10008979c32898e01f5cf561b87f1402850f
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-10 18:45:38 -0400

    added redux and router. tested router for home page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,22 @@
+import store from "../store";
+import { Provider } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Home/Home";
+
 function App() {
 	return (
+		//<Provider store={store}>
 		<div>
+			<Switch>
+				{/* <Route path="/login" render={(props) => <LoginPage {...props} login={true} />} /> */}
+				{/* <Route path="/logout" render={(props) => <LoginPage {...props} login={false} />} /> */}
+				<Route path="/" exact component={Home} />
+				{/* <Route path="/otherUserProfilePage" component={OtherUserProfilePage} /> */}
+				{/* <Route path="/searchUserPage" component={SearchUserPage} /> */}
+			</Switch>
 			<h1>Hello World!</h1>
 		</div>
+		//</Provider>
 	);
 }
 
