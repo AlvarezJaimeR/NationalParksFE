aebbc5e78a2def5bf5f64dd765e56b276c651a27
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-14 16:42:22 -0400

    updated the main park page to use app context.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,65 @@
 import NavBar from "../NavBar/NavBar";
-import { getPark, filterNationalPark } from "../../actions/parkAction";
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useAppContext } from "../../libs/contextLib";
+import axios from "axios";
 import "./Main.css";
 
-class Main extends Component {
+const Main = () => {
+	const { loggedInUser, setLoggedInUser, isAuthenticated } = useAppContext();
+	const [parks, setParks] = useState([]);
+
+	useEffect(() => {
+		getParks();
+	}, []);
+
+	async function getParks() {
+		await axios
+			.get(
+				`https://developer.nps.gov/api/v1/parks?&api_key=t6gYQ5xCA0LgivhLEO2zbVfKa3pWcZcdix1CPozR`
+			)
+			.then((response) => {
+				console.log(response);
+				setParks(response.data.data);
+			})
+			.catch((error) => {
+				console.log(error);
+				alert(error.response.data);
+			});
+	}
+
+	function nationalParkOnly() {
+		console.log("filter for only National Parks");
+		console.log(parks);
+		const tempParks = parks.filter(
+			(parks) => parks.designation === "National Park"
+		);
+		console.log("tempParks", tempParks);
+		setParks(tempParks);
+	}
+
+	return parks.length > 0 ? (
+		<div>
+			<NavBar tabActive="1" />
+			<h1 className="main">National Parks!</h1>
+			<div className="container">
+				<div className="row">
+					{parks.map((park, index) => (
+						<div key={index} className="card" style={{ width: "11em" }}>
+							<a href="/specificPark" className="card-body">
+								{park.name}
+							</a>
+						</div>
+					))}
+				</div>
+			</div>
+			<button onClick={() => nationalParkOnly()}>Filter</button>
+		</div>
+	) : (
+		<h1>Loading...</h1>
+	);
+};
+
+/* class Main extends Component {
 	componentDidMount() {
 		console.log("get parks!");
 		this.props.getPark();
@@ -51,6 +105,6 @@ class Main extends Component {
 
 const mapStateToProps = (state) => ({
 	parks: state.park.items,
-});
+}); */
 
-export default connect(mapStateToProps, { getPark, filterNationalPark })(Main);
+export default Main;
