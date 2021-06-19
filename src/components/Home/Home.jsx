19b0fa8c8409c9479516ca38b1b2db0fafd93d95
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-19 15:20:36 -0400

    updated styling of the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,9 +20,11 @@ const Home = () => {
 				console.log(response.data.data);
 				const tempParks = response.data.data.filter(
 					(parks) =>
-						parks.parkCode === "npsa" ||
-						parks.parkCode === "neri" ||
-						parks.parkCode === "redw"
+						parks.parkCode === "glac" ||
+						parks.parkCode === "whsa" ||
+						parks.parkCode === "kefj" ||
+						parks.parkCode === "acad" ||
+						parks.parkCode === "brca"
 				);
 				setParks(tempParks);
 				console.log(tempParks);
@@ -36,19 +38,20 @@ const Home = () => {
 		<div>
 			{parks.length > 0 ? (
 				<div>
-					<NavBar tabActive="0" />
-					<h1 className="home">Hello Home!</h1>
-					<div className="row row-cols-1">
+					<div className="container">
+						<NavBar tabActive="0" />
+					</div>
+					<div className="home container">
 						<div className="col">
-							<h4 className="text-center">Reasons to be here</h4>
+							<h4 className="text-center">National Parks to visit!</h4>
 						</div>
 					</div>
-					<div className="row ">
+					<div className="container">
 						<div className="col w-100 ">
 							<div
 								id="carouselExampleCaptions"
-								className="carousel slide"
-								data-bs-ride="carousel">
+								className="carousel slide carousel-fade"
+								data-bs-ride="carousel fade">
 								<div className="carousel-indicators">
 									<button
 										type="button"
@@ -67,39 +70,86 @@ const Home = () => {
 										data-bs-target="#carouselExampleCaptions"
 										data-bs-slide-to="2"
 										aria-label="Slide 3"></button>
+									<button
+										type="button"
+										data-bs-target="#carouselExampleCaptions"
+										data-bs-slide-to="3"
+										aria-label="Slide 4"></button>
+									<button
+										type="button"
+										data-bs-target="#carouselExampleCaptions"
+										data-bs-slide-to="4"
+										aria-label="Slide 5"></button>
 								</div>
 								<div className="carousel-inner">
 									<div className="carousel-item active">
-										<img
-											src={parks[0].images[0].url}
-											className="d-block w-100 h500 carousel-image"
-											alt="test"
-										/>
-										<div className="carousel-caption d-none d-md-block">
-											<h5>DISCOVER</h5>
-											<p>Find something new!</p>
+										<a href={parks[0].url}>
+											<img
+												src={parks[0].images[0].url}
+												className="w-100 carousel-image"
+												alt={parks[0].images[0].altText}
+											/>
+										</a>
+										<div className="carousel-caption ">
+											<h6>
+												{parks[0].fullName} {" , "} {parks[0].states}
+											</h6>
 										</div>
 									</div>
 									<div className="carousel-item">
-										<img
-											src={parks[1].images[0].url}
-											className="d-block w-100 carousel-image"
-											alt="temp"
-										/>
-										<div className="carousel-caption d-none d-md-block">
-											<h5>RECOGNIZE</h5>
-											<p>Music is the source of life.</p>
+										<a href={parks[1].url}>
+											<img
+												src={parks[1].images[0].url}
+												className="w-100 carousel-image"
+												alt={parks[1].images[0].altText}
+											/>
+										</a>
+										<div className="carousel-caption ">
+											<h6>
+												{parks[1].fullName} {" , "} {parks[1].states}
+											</h6>
 										</div>
 									</div>
 									<div className="carousel-item">
-										<img
-											src={parks[2].images[0].url}
-											className="d-block w-100 h500 carousel-image"
-											alt="alt"
-										/>
-										<div className="carousel-caption d-none d-md-block">
-											<h5>ENJOY</h5>
-											<p>Have fun making friends and finding new music.</p>
+										<a href={parks[2].url}>
+											<img
+												src={parks[2].images[4].url}
+												className="w-100 carousel-image"
+												alt={parks[2].images[4].altText}
+											/>
+										</a>
+										<div className="carousel-caption d">
+											<h6>
+												{parks[2].fullName} {" , "} {parks[2].states}
+											</h6>
+										</div>
+									</div>
+									<div className="carousel-item">
+										<a href={parks[3].url}>
+											<img
+												src={parks[3].images[0].url}
+												className="w-100 carousel-image"
+												alt={parks[3].images[0].altText}
+											/>
+										</a>
+										<div className="carousel-caption d">
+											<h6>
+												{parks[3].fullName} {" , "} {parks[3].states}
+											</h6>
+										</div>
+									</div>
+									<div className="carousel-item">
+										<a href={parks[4].url}>
+											<img
+												src={parks[4].images[0].url}
+												className="w-100 carousel-image"
+												alt={parks[4].images[0].altText}
+											/>
+										</a>
+										<div className="carousel-caption d">
+											<h6>
+												{parks[4].fullName} {" , "} {parks[4].states}
+											</h6>
 										</div>
 									</div>
 								</div>
@@ -118,9 +168,7 @@ const Home = () => {
 									type="button"
 									data-bs-target="#carouselExampleCaptions"
 									data-bs-slide="next">
-									<span
-										className="carousel-control-next-icon"
-										aria-hidden="true"></span>
+									<span className="carousel-control-next-icon"></span>
 									<span className="visually-hidden">Next</span>
 								</button>
 							</div>
