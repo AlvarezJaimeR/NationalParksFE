dec99deda3bc7cdbcbebfe2f19a237172a23a064
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-04 14:41:34 -0500

    specific traveler page can display all of the reviews that the user posted.

--- src/components/SpecificTraveler/SpecificTraveler.jsx
+++ src/components/SpecificTraveler/SpecificTraveler.jsx
@@ -8,30 +8,11 @@ const SpecificTraveler = (props) => {
 	console.log(props.location.state);
 	const { loggedInUser, totalUsers, parks } = useAppContext();
 	console.log(props.location.state.users[props.location.state.index]);
-	const [filteredVisit, setFilteredVisit] = useState([]);
-	const [wishedVisit, setWishedVisit] = useState([]);
 
 	useEffect(() => {
 		console.log("passing");
-		const filterVisit = parks.filter((park) => {
-			for (
-				let i = 0;
-				i <
-				props.location.state.users[props.location.state.index].visitedParks
-					.length;
-				i++
-			) {
-				if (
-					park.fullName ===
-					props.location.state.users[props.location.state.index].visitedParks[i]
-						.text
-				) {
-					return park;
-				}
-			}
-		});
-		setFilteredVisit(filterVisit);
 	}, []);
+
 	return (
 		<div>
 			<NavBar tabActive="2" />
@@ -106,6 +87,27 @@ const SpecificTraveler = (props) => {
 					) : (
 						<div>
 							<p>This traveler has not been able to visit a park.</p>
+						</div>
+					)}
+				</div>
+			</div>
+			<div className="container">
+				<h3>Posts:</h3>
+				<div className="row">
+					{props.location.state.users[props.location.state.index].posts.length >
+					0 ? (
+						<div className="wrapper">
+							{props.location.state.users[props.location.state.index].posts.map(
+								(post, index) => (
+									<div key={index}>
+										<p className="park-text">{post.text}</p>
+									</div>
+								)
+							)}
+						</div>
+					) : (
+						<div>
+							<p>This traveler hasn't posted any reviews yet.</p>
 						</div>
 					)}
 				</div>
